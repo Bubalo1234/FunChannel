72f23aca5798788f308c25514d0b34ccf480cad8
Isabella <80659275+Bubalo1234@users.noreply.github.com>
2022-02-18 19:45:28 -0800

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -1,6 +1,3 @@
-var myIdek = [];
-var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 5, 3, 1, 2, 2, 2, 3];
-
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(255);
@@ -9,37 +6,37 @@ function setup() {
 		myIdek.push(new Idek(1));
 	}
 	
-		for(var i = 0; i < myIdek.length; i++){
+	for(var i = 0; i < myIdek.length; i++){
+		var ex = random(10, width-10);
+		var ey = random(10, height-10);
 		var redBlah = random(1, 205);
-		var greenBlah = random(1, 205);
-		var blueBlah = random(1, 205);
-		var mouth = random(10, 90)
-		var eye = random(5, 40)
-		var siz = random (1, 200)
-		var hatW = random(10, 50)
-		var hatH = random(20, 100)
+	var greenBlah = random(1, 205);
+	var blueBlah = random(1, 205);
+	var mouth = random(10, 90)
+	var eye = random(5, 40)
+	var siz = random (1, 200)
+	var hatW = random(10, 50)
+	var hatH = random(20, 100)
 	
-		noStroke()
-		fill(redBlah, greenBlah, blueBlah);
-		ellipse(this.x, this.y, 100, 100);
-		fill(redBlah+50, greenBlah+50, blueBlah+50);
-		ellipse(this.x, this.y, mouth, mouth);
-		fill(redBlah, greenBlah, blueBlah);
-		ellipse(this.x, this.y, mouth-10, mouth-10);
-		rect(this.x-50, this.y-50, 100, 50);
-		fill(redBlah+50, greenBlah+50, blueBlah+50);
-		ellipse(this.x-27, this.y-27, eye, eye);
-		ellipse(this.x+27, this.y-27, eye, eye);
-		textSize(25)
-		text("budee "+(how+1), this.x-50, this.y+75)
+	noStroke()
+	fill(redBlah, greenBlah, blueBlah);
+  ellipse(ex, ey, 100, 100);
+	fill(redBlah+50, greenBlah+50, blueBlah+50);
+	ellipse(ex, ey, mouth, mouth);
+	fill(redBlah, greenBlah, blueBlah);
+	ellipse(ex, ey, mouth-10, mouth-10);
+	rect(ex-50, ey-50, 100, 50);
+	fill(redBlah+50, greenBlah+50, blueBlah+50);
+	ellipse(ex-27, ey-27, eye, eye);
+	ellipse(ex+27, ey-27, eye, eye);
+	textSize(25)
+	text("budee "+(i+1), ex-50, ey+75)
 	
-		fill(redBlah+50, greenBlah+50, blueBlah+50);
-		triangle(this.x-hatW, this.y-50, this.x+hatW, this.y-50, this.x, this.y-50-hatH) 
-}
-	}
+	fill(redBlah+50, greenBlah+50, blueBlah+50);
+	triangle(ex-hatW, ey-50, ex+hatW, ey-50, ex, ey-50-hatH);
 }
 
 function draw() {
-	
+}
 
 }
