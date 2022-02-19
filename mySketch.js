244ce0f7b06b0add07caac574780988213d29ebd
Isabella <80659275+Bubalo1234@users.noreply.github.com>
2022-02-18 19:10:24 -0800

    Adding main file for Budee toy.
    Adding main file for Budee website toy.  Pairs with idek.js.

---
+++ mySketch.js
@@ -0,0 +1,20 @@
+var myIdek = [];
+var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 5, 3, 1, 2, 2, 2, 3];
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(255);
+	
+	for(var i = 0; i < random(1, 10); i++){
+		myIdek.push(new Idek(1));
+	}
+	
+	for(var i = 0; i < myIdek.length; i++){
+		myIdek[i].display(i);
+	}
+}
+
+function draw() {
+	
+
+}
