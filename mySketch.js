ffb5931079f894f2cbf0bc751a802cbecbc3ec70
Isabella <80659275+Bubalo1234@users.noreply.github.com>
2022-02-18 19:40:27 -0800

    Importing Idek.js into mySketch.js
    Will fix bug?

--- mySketch.js
+++ mySketch.js
@@ -9,8 +9,33 @@ function setup() {
 		myIdek.push(new Idek(1));
 	}
 	
-	for(var i = 0; i < myIdek.length; i++){
-		myIdek[i].display(i);
+		for(var i = 0; i < myIdek.length; i++){
+		var redBlah = random(1, 205);
+		var greenBlah = random(1, 205);
+		var blueBlah = random(1, 205);
+		var mouth = random(10, 90)
+		var eye = random(5, 40)
+		var siz = random (1, 200)
+		var hatW = random(10, 50)
+		var hatH = random(20, 100)
+	
+		noStroke()
+		fill(redBlah, greenBlah, blueBlah);
+		ellipse(this.x, this.y, 100, 100);
+		fill(redBlah+50, greenBlah+50, blueBlah+50);
+		ellipse(this.x, this.y, mouth, mouth);
+		fill(redBlah, greenBlah, blueBlah);
+		ellipse(this.x, this.y, mouth-10, mouth-10);
+		rect(this.x-50, this.y-50, 100, 50);
+		fill(redBlah+50, greenBlah+50, blueBlah+50);
+		ellipse(this.x-27, this.y-27, eye, eye);
+		ellipse(this.x+27, this.y-27, eye, eye);
+		textSize(25)
+		text("budee "+(how+1), this.x-50, this.y+75)
+	
+		fill(redBlah+50, greenBlah+50, blueBlah+50);
+		triangle(this.x-hatW, this.y-50, this.x+hatW, this.y-50, this.x, this.y-50-hatH) 
+}
 	}
 }
 
